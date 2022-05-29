1ba24319e3db571011ed84fd7df067946d1b1500
SahithiShresta4 <99709584+SahithiShresta4@users.noreply.github.com>
2022-05-29 07:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import requests
 
 app = flask.Flask(__name__, template_folder='templates')
 
-df2 = pd.read_csv('./model/tmdb.csv')
+df2 = pd.read_csv('./tmdb.csv')
 
 count = CountVectorizer(stop_words='english')
 count_matrix = count.fit_transform(df2['soup'])
