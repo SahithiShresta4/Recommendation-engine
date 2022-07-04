65409b6078b0c0f38efd50d90a039d654c1913a4
SahithiShresta4 <99709584+SahithiShresta4@users.noreply.github.com>
2022-07-04 17:09:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
 import flask
 import json
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from abcd import KNearestNeighbours
 from operator import itemgetter
 
-import requests
-
 app = flask.Flask(__name__, template_folder='templates')
-
-df2 = pd.read_csv('./tmdb.csv')
-
-count = CountVectorizer(stop_words='english')
-count_matrix = count.fit_transform(df2['soup'])
-
-cosine_sim2 = cosine_similarity(count_matrix, count_matrix)
-
-df2 = df2.reset_index()
-indices = pd.Series(df2.index, index=df2['title'])
-all_titles = [df2['title'][i] for i in range(len(df2['title']))]
 
 with open(r'data.json', 'r+', encoding='utf-8') as f:
     data = json.load(f)
@@ -38,22 +22,6 @@ def knn(test_point, k):
     for i in model.indices:
         table.append([movie_titles[i][0], movie_titles[i][2]])
     return table
-
-
-def get_recommendations(title):
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:11]
-    movie_indices = [i[0] for i in sim_scores]
-    tit = df2['title'].iloc[movie_indices]
-    dat = df2['release_date'].iloc[movie_indices]
-    return_df = pd.DataFrame(columns=['Title', 'Year'])
-    return_df['Title'] = tit
-    return_df['Year'] = dat
-    return return_df
-
 
 # Set up the main route
 @app.route('/', methods=['GET', 'POST'])
